c674228fee676a0a5a4afbd7874770277af5d14f
TheNurse <tdz_7@hotmail.com>
2016-06-15 17:35:27 +0700

    link to home page by url (app.js)

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('fitkeeper', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,15 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function ($stateProvider,$urlRouterProvider) {
+  $stateProvider
+    .state('homepage', {
+      url: '/home',
+      templateUrl: 'template/homepage.html'
+    })
+
+  $urlRouterProvider.otherwise('/home')
+
 })
